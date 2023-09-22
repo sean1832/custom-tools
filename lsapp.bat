60c93134c6d5f04b5d4bd05bad437c52471b5c9d
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-09-23 05:16:18 +1000

    add lsapp.bat to new scripts

--- lsapp.bat
+++ lsapp.bat
@@ -1,4 +1,28 @@
 @echo off
-pushd "C:\_PATH\custom-tools"
-python src\listapp.py %*
+setlocal enabledelayedexpansion
+
+:: change here
+set PROJECT_NAME=cli-list-installed-apps
+set GIT_URL=https://github.com/sean1832/cli-list-installed-apps.git
+
+set ROOT_DIR=%~dp0
+set TOOL_DIR=%ROOT_DIR%\code\%PROJECT_NAME%
+set VENV_DIR=%TOOL_DIR%\venv
+
+:: check if the tool directory exists or not, if exist install, else update
+if exist %TOOL_DIR% (
+    echo Updating %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/update.py %TOOL_DIR%
+) else (
+    echo Installing %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/install.py %GIT_URL%
+)
+
+:: activate the virtual environment
+if exist %VENV_DIR%\Scripts\activate.bat (
+    call "%VENV_DIR%\Scripts\activate"
+)
+
+:: run the tool
+python %TOOL_DIR%/list-installed-apps.py %*
 popd
