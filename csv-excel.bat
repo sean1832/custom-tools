4c11cd1313f2dba27149cb5d9fb3ce573c7aca22
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-09-23 05:06:00 +1000

    update csv-excel.bat to use new script

--- csv-excel.bat
+++ csv-excel.bat
@@ -1,4 +1,26 @@
 @echo off
-pushd "C:\_PATH\custom-tools"
-python src\csv-excel.py %*
+setlocal enabledelayedexpansion
+
+:: change here
+set PROJECT_NAME=cli-csv-excel
+set GIT_URL=https://github.com/sean1832/cli-csv-excel.git
+
+set ROOT_DIR=%~dp0
+set TOOL_DIR=%ROOT_DIR%\code\%PROJECT_NAME%
+set VENV_DIR=%TOOL_DIR%\venv
+
+:: check if the tool directory exists or not, if exist install, else update
+if exist %TOOL_DIR% (
+    echo Updating %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/update.py %TOOL_DIR%
+) else (
+    echo Installing %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/install.py %GIT_URL%
+)
+
+:: activate the virtual environment
+call "%VENV_DIR%\Scripts\activate"
+
+:: run the tool
+python %TOOL_DIR%/csv-excel.py %*
 popd
