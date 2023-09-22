539dabbf159c9e4c6d7eda50d7e929e08eb5c6b9
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-09-23 04:48:41 +1000

    modify autocite and convert-img to use new script

--- autocite.bat
+++ autocite.bat
@@ -1,74 +1,25 @@
 @echo off
 setlocal enabledelayedexpansion
-set TOOL_DIR=C:\_PATH\custom-tools\src\python-autocite
-set VENV_DIR=%TOOL_DIR%\venv
+
+:: change here
+set PROJECT_NAME=python-autocite
 set GIT_URL=https://github.com/sean1832/python-autocite.git
 
-if not exist "%TOOL_DIR%" (
-    echo python-autocite does not exist.
-    set /p user_input="Do you want to clone from %GIT_URL% (y/n)? "
-    set user_input=!user_input:~0,1!
-    if /i "!user_input!" neq "y" (
-        echo Exiting...
-        exit /b 1
-    )
-    git clone %GIT_URL% %TOOL_DIR%
-    if errorlevel 1 (
-        echo Failed to clone python-autocite. Exiting...
-        exit /b 1
-    )
+set ROOT_DIR=%~dp0
+set TOOL_DIR=%ROOT_DIR%\code\%PROJECT_NAME%
+set VENV_DIR=%TOOL_DIR%\venv
+
+:: check if the tool directory exists or not, if exist install, else update
+if exist %TOOL_DIR% (
+    echo Updating %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/update.py %TOOL_DIR%
 ) else (
-    pushd "%TOOL_DIR%"
-    git remote update
-    set LOCAL_STATUS=
-    for /f "tokens=*" %%a in ('git status -uno ^| find "Your branch is behind"') do set LOCAL_STATUS=%%a
-    if not "%LOCAL_STATUS%"=="" (
-        set /p user_input="Your local branch is behind the remote branch. Do you want to pull the latest changes (y/n)? "
-        set user_input=!user_input:~0,1!
-        if /i "!user_input!" neq "y" (
-            echo Exiting...
-            exit /b 1
-        )
-        git pull
-        if errorlevel 1 (
-            echo Failed to pull the latest changes. Exiting...
-            exit /b 1
-        )
-    )
-    popd
+    echo Installing %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/install.py %GIT_URL%
 )
 
-if not exist "%VENV_DIR%" (
-    echo Virtual environment does not exist.
-    set /p user_input="Do you want to create a virtual environment (y/n)? "
-    if /i "%user_input%" neq "y" (
-        echo Exiting...
-        exit /b 1
-    )
-    python -m venv %VENV_DIR%
-    if errorlevel 1 (
-        echo Failed to create virtual environment. Exiting...
-        exit /b 1
-    )
-)
-
+:: activate the virtual environment
 call "%VENV_DIR%\Scripts\activate"
 
-python -c "import pkg_resources; pkg_resources.get_distribution('python_autocite')"
-if errorlevel 1 (
-    echo python_autocite is not installed.
-    set /p user_input="Do you want to install python_autocite (y/n)? "
-    if /i "%user_input%" neq "y" (
-        echo Exiting...
-        exit /b 1
-    )
-    pushd "%TOOL_DIR%"
-    python setup.py install
-    if errorlevel 1 (
-        echo Failed to install python_autocite. Exiting...
-        exit /b 1
-    )
-    popd
-)
-cls
+:: run the tool
 autocite %*
--- convert-img.bat
+++ convert-img.bat
@@ -1,5 +1,26 @@
 @echo off
-pushd "C:\_PATH\custom-tools"
-call .\src\convert-img\\venv\Scripts\activate
-python src\convert-img\convert-img.py %*
+setlocal enabledelayedexpansion
+
+:: change here
+set PROJECT_NAME=cli-convert-img
+set GIT_URL=https://github.com/sean1832/cli-convert-img.git
+
+set ROOT_DIR=%~dp0
+set TOOL_DIR=%ROOT_DIR%\code\%PROJECT_NAME%
+set VENV_DIR=%TOOL_DIR%\venv
+
+:: check if the tool directory exists or not, if exist install, else update
+if exist %TOOL_DIR% (
+    echo Updating %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/update.py %TOOL_DIR%
+) else (
+    echo Installing %PROJECT_NAME%...
+    python %ROOT_DIR%/utilities/install.py %GIT_URL%
+)
+
+:: activate the virtual environment
+call "%VENV_DIR%\Scripts\activate"
+
+:: run the tool
+python %TOOL_DIR%/convert-img.py %*
 popd
