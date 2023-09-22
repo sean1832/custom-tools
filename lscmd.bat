260624a518f474083714b4a107f8d4a0f26dd51c
Zeke Zhang <84948038+sean1832@users.noreply.github.com>
2023-09-23 05:30:23 +1000

    ad lscmd to utilities functions

--- lscmd.bat
+++ lscmd.bat
@@ -1,4 +1,13 @@
 @echo off
-pushd "C:\_PATH\custom-tools"
-python src\listcmd.py
+setlocal enabledelayedexpansion
+
+:: change here
+set PROJECT_NAME=cli-csv-excel
+set GIT_URL=https://github.com/sean1832/cli-csv-excel.git
+
+set ROOT_DIR=%~dp0
+set TOOL_DIR=%ROOT_DIR%\code\%PROJECT_NAME%
+set VENV_DIR=%TOOL_DIR%\venv
+
+python %ROOT_DIR%\utilities\lscmd.py -d %ROOT_DIR%
 popd
---
+++ utilities/lscmd.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+
+def list_files(directory):
+    try:
+        # Get all files and subdirectories in the directory
+        entries = os.listdir(directory)
+
+        # Filter out subdirectories and only keep files
+        files_only = [entry for entry in entries if os.path.isfile(os.path.join(directory, entry))]
+
+        outputs = []
+        for file in files_only:
+            if file.endswith('.bat'):
+                splited = file.split('.')
+                file = splited[0]
+                outputs.append(file)
+
+        return outputs
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+    
+def main():
+    parser = argparse.ArgumentParser(description='List custom commands')
+    parser.add_argument('-d', '--directory', type=str, default='C:\_PATH\custom-tools', help='Directory to list the files from')
+    args = parser.parse_args()
+
+    files = list_files(args.directory)
+    print("Custom commands:\n")
+    print("====================================\n")
+    for file in files:
+        print(file)
+    print("\n====================================")
+
+if __name__ == '__main__':
+    main()
